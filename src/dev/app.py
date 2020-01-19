d14656b419a77edef30b055b2bb72554b5037cf6
pravin <pravin.boppuri@gmail.com>
2020-01-19 22:05:27 +0000

    applying branch protection

--- src/dev/app.py
+++ src/dev/app.py
@@ -1,16 +1,29 @@
 import json
 from flask import Flask
-from flask import request, abort
+from flask import request, abort, send_from_directory
 import os
 import hmac
 import hashlib
 
+
 GITHUB_SECRET = os.environ["GITHUB_TOKEN"]
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def api():
     return "hello world"
+
+@app.route("/health")
+def health():
+        return (
+            "ok",
+            200,
+            {"Content-Type": "text/plain; charset=utf-8", "Cache-Control": "no-cache"},
+        )
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 
 @app.route('/github', methods=['POST'])
@@ -30,12 +43,13 @@ def listener():
         abort(400, "Invalid signature")
 
     if request.headers['Content-Type'] == 'application/json':
+        # url = os.path.join(os.path.dirname(__file__), "event_store")
         event = json.dumps(request.json)
-        print (event)
         return event
-
 
 if __name__ == "__main__":
 
     app.run(debug=True)
+
+
 
